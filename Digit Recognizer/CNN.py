ea73654ddf7be009c14177ea28cfdae88f8e1ccd
Sina  Molavipour <49862654+smolavipour@users.noreply.github.com>
2021-01-05 22:56:34 +0100

    0.98 % accuracy in Kaggle

--- Digit Recognizer/CNN.py
+++ Digit Recognizer/CNN.py
@@ -1,15 +1,12 @@
 import numpy as np
 import pandas as pd
 
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
+
 from keras.utils import to_categorical
 
-import tensorflow as tf
+from tensorflow.keras import layers, models
 
-from sklearn.model_selection import KFold
-from sklearn import preprocessing
+import matplotlib.pyplot as plt
 
 def data_preparation(D,isTest=False):
     out=[]
@@ -50,46 +47,39 @@ batch_size = 128
 n_input = 28
 n_classes = 10
 
-x = tf.placeholder("float", [None, 28, 28, 1])
-y = tf.placeholder("float", [None, n_classes])
 
-def conv2d(x, W, b, stride = 1):
-    x = tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "SAME")
-    x = tf.nn.bias_add(x, b)
-    return tf.nn.relu(x)
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
-def maxpool2d(x, k = 2):
-    return tf.nn.max_pool(x, ksize = [1, k , k , 1], strides = [1, k , k, 1], padding = "SAME")
 
-weights = {
-    'wc1' : tf.get_variable('W0', shape = (3, 3, 1, 32), initializer = tf.contrib.layers.xavier_initializer()),
-    'wc2' : tf.get_variable('W1', shape = (3, 3, 32, 64), initializer = tf.contrib.layers.xavier_initializer()),
-    'wc3' : tf.get_variable('W2', shape = (3, 3, 64, 128), initializer = tf.contrib.layers.xavier_initializer()),
-    'wd1' : tf.get_variable('W3', shape = (4 * 4 * 128, 128), initializer = tf.contrib.layers.xavier_initializer()),
-    'out' : tf.get_variable('W4', shape = (128, n_classes), initializer = tf.contrib.layers.xavier_initializer())
-}
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
 
-biases = {
-    'bc1': tf.get_variable('B0', shape = (32), initializer = tf.contrib.layers.xavier_initializer()),
-    'bc2': tf.get_variable('B1', shape = (64), initializer = tf.contrib.layers.xavier_initializer()),
-    'bc3': tf.get_variable('B2', shape = (128), initializer = tf.contrib.layers.xavier_initializer()),
-    'bd1': tf.get_variable('B3', shape = (128), initializer = tf.contrib.layers.xavier_initializer()),
-    'out': tf.get_variable('B4', shape = (10), initializer = tf.contrib.layers.xavier_initializer()),
-}
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
-def conv_net(x, weights, biases):
-    conv1 = conv2d(x, weights['wc1'], biases['bc1'])
-    conv1 = maxpool2d(conv1, k = 2)
-    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
-    conv2 = maxpool2d(conv2, k = 2)
-    conv3 = conv2d(conv2, weights['wc3'], biases['bc3'])
-    conv3 = maxpool2d(conv3, k = 2)
-    fc1 = tf.reshape(conv3, [-1, weights['wd1'].get_shape().as_list()[0]])
-    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
-    fc1 = tf.nn.relu(fc1)
-    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
-    return out
+history = model.fit(X_train, Y_train, epochs=epochs, 
+                    validation_data=(X_train, Y_train))
 
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
+out=model.predict(X_test.astype(np.float64))
+out=np.argmax(out,axis=1)
+id_test=np.arange(0,28000)+1
+d={'ImageId':id_test,'Label':out}
+pd.DataFrame(d).to_csv('out_CNN.csv',index=None)
 
 #Predict
 #out=model.predict(X_test)
